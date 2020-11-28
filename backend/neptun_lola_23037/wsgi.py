a6280a30a2c2be87e1ff36beaf1335824a9c0a34
Crowdbotics <team@crowdbotics.com>
2020-11-28 17:32:54 +0000

    Update models

--- backend/neptun_lola_23037/wsgi.py
+++ backend/neptun_lola_23037/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neptun_lola_23037.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neptun_lola_23037.settings")
 
 application = get_wsgi_application()
